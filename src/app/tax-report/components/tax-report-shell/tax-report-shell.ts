8052190eb653e6480eae1573668f23cca533dd5e
Richard Zhuno <967968+Zhunio@users.noreply.github.com>
2023-06-18 09:23:00 -0400

    Fix tax report reload

--- src/app/tax-report/components/tax-report-shell/tax-report-shell.ts
+++ src/app/tax-report/components/tax-report-shell/tax-report-shell.ts
@@ -38,10 +38,18 @@ import { filter, map, switchMap, take, tap } from 'rxjs';
       <ng-container matColumnDef="rowActions">
         <mat-header-cell *matHeaderCellDef></mat-header-cell>
         <mat-cell *matCellDef="let row">
-          <button mat-icon-button color="primary" (click)="onDownloadRowActionClicked(row)">
+          <button
+            mat-icon-button
+            color="primary"
+            (click)="$event.preventDefault(); onDownloadRowActionClicked(row)"
+          >
             <mat-icon class="scale-90">cloud_download</mat-icon>
           </button>
-          <button mat-icon-button color="warn" (click)="onDeleteTaxReportClicked(row)">
+          <button
+            mat-icon-button
+            color="warn"
+            (click)="$event.preventDefault(); onDeleteTaxReportClicked(row)"
+          >
             <mat-icon class="scale-90">delete</mat-icon>
           </button>
         </mat-cell>
