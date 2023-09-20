9b3e1b47b7720093d22ee1b33007f448d68b449d
Richard Zhunio <967968+Zhunio@users.noreply.github.com>
2023-09-20 17:00:15 -0400

    Add login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,34 +3,36 @@ import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../shared/services/auth/auth.service';
+import { SessionStorageService } from '../shared/services/session-storage/session-storage.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   template: `
     <!-- <div class="grid grid-cols-1 gap-3"> -->
-      <!-- USERNAME -->
-      <mat-form-field>
-        <mat-label>Username</mat-label>
-        <input type="text" matInput [(ngModel)]="username" placeholder="john" />
-      </mat-form-field>
+    <!-- USERNAME -->
+    <mat-form-field>
+      <mat-label>Username</mat-label>
+      <input type="text" matInput [(ngModel)]="username" placeholder="john" />
+    </mat-form-field>
 
-      <!-- PASSWORD -->
-      <mat-form-field>
-        <mat-label>Password</mat-label>
-        <input type="password" matInput [(ngModel)]="password" placeholder="doe" />
-      </mat-form-field>
+    <!-- PASSWORD -->
+    <mat-form-field>
+      <mat-label>Password</mat-label>
+      <input type="password" matInput [(ngModel)]="password" placeholder="doe" />
+    </mat-form-field>
 
-      <!-- LOGIN -->
-      <button
-        [disabled]="!username || !password"
-        mat-raised-button
-        color="primary"
-        class="flex-1"
-        (click)="login()"
-      >
-        Login
-      </button>
+    <!-- LOGIN -->
+    <button
+      [disabled]="!username || !password"
+      mat-raised-button
+      color="primary"
+      class="flex-1"
+      (click)="login()"
+    >
+      Login
+    </button>
     <!-- </div> -->
   `,
   styles: [
@@ -46,12 +48,26 @@ import { MatInputModule } from '@angular/material/input';
     `,
   ],
   imports: [FormsModule, MatFormFieldModule, MatButtonModule, MatInputModule],
+  providers: [AuthService, SessionStorageService]
 })
 export class LoginComponent {
+  constructor(private readonly authService: AuthService) {}
+
+  isLoading = false;
+
   username = '';
   password = '';
 
   login() {
-    console.log(this.username, this.password);
+    this.isLoading = true;
+
+    this.authService.login({ username: this.username, password: this.password }).subscribe({
+      next: () => {
+        this.isLoading = false;
+      },
+      error: () => {
+        this.isLoading = false;
+      },
+    });
   }
 }
